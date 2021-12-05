469c979429578c7fdea29cfe5e95b193d0136ae8
Elvan H <elvanhd@gmail.com>
2021-12-05 21:06:56 +0700

    component updates example

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import Button from './components/UI/Button/Button';
 
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  console.log('App rendered');
+
+  const toggleParagraphHandler = () => {
+    setShowParagraph((prevState) => !prevState);
+  };
+
   return (
     <div className='app'>
       <h1>Hi there!</h1>
+      {showParagraph && <p>This is new!</p>}
+      <Button onClick={toggleParagraphHandler}>Toggle paragraph</Button>
     </div>
   );
 }
